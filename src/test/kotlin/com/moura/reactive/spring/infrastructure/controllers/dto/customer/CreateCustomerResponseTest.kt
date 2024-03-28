501d265aeb136fa3d86190bbd55dcaf3aa524346
Ismael Moura <ismaeldesousa2014@outlook.com>
2024-03-28 19:46:39 -0300

    add missing unit test annotations

--- src/test/kotlin/com/moura/reactive/spring/infrastructure/controllers/dto/customer/CreateCustomerResponseTest.kt
+++ src/test/kotlin/com/moura/reactive/spring/infrastructure/controllers/dto/customer/CreateCustomerResponseTest.kt
@@ -1,9 +1,11 @@
 package com.moura.reactive.spring.infrastructure.controllers.dto.customer
 
+import com.moura.reactive.spring.UnitTest
 import com.moura.reactive.spring.fixture.DeserializationTesterModule
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
+@UnitTest
 class CreateCustomerResponseTest : DeserializationTesterModule() {
 
     @Test
