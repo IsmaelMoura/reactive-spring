e5fa7b137e7e9853b368887d463eec601981f0a6
Ismael Moura <ismaeldesousa2014@outlook.com>
2024-04-03 21:56:41 -0300

    feat: add custom queries

--- src/main/kotlin/com/moura/reactive/spring/infrastructure/persistence/repository/CustomerRepository.kt
+++ src/main/kotlin/com/moura/reactive/spring/infrastructure/persistence/repository/CustomerRepository.kt
@@ -1,6 +1,26 @@
 package com.moura.reactive.spring.infrastructure.persistence.repository
 
 import com.moura.reactive.spring.infrastructure.persistence.entity.CustomerEntity
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface CustomerRepository : CoroutineCrudRepository<CustomerEntity, Long>
+interface CustomerRepository : CoroutineCrudRepository<CustomerEntity, Long> {
+
+    @Query(
+        """
+            UPDATE reactive_spring_schema.customers
+            SET name = ?1
+            WHERE reactive_spring_schema.customers.id == ?2
+        """
+    )
+    suspend fun updateNameById(name: String, customerId: Long)
+
+    @Query(
+        """
+            UPDATE reactive_spring_schema.customers
+            SET email = ?1
+            WHERE reactive_spring_schema.customers.id == ?2
+        """
+    )
+    suspend fun updateEmailById(email: String, customerId: Long)
+}
--- src/main/kotlin/com/moura/reactive/spring/infrastructure/persistence/repository/ProductRepository.kt
+++ src/main/kotlin/com/moura/reactive/spring/infrastructure/persistence/repository/ProductRepository.kt
@@ -1,6 +1,27 @@
 package com.moura.reactive.spring.infrastructure.persistence.repository
 
 import com.moura.reactive.spring.infrastructure.persistence.entity.ProductEntity
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface ProductRepository : CoroutineCrudRepository<ProductEntity, Long>
+interface ProductRepository : CoroutineCrudRepository<ProductEntity, Long> {
+
+    @Query(
+        """
+            UPDATE reactive_spring_schema.products 
+            SET name = ?1 
+            WHERE reactive_spring_schema.products.id == ?2
+        """
+    )
+    suspend fun updateNameById(email: String, productId: Long)
+
+    @Query(
+        """
+            UPDATE reactive_spring_schema.products 
+            SET name = ?1 
+            WHERE reactive_spring_schema.products.id == ?2
+        """
+    )
+    suspend fun updatePriceById(name: String, customerId: Long)
+
+}
